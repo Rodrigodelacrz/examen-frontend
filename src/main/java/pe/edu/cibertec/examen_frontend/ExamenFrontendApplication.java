5f54bbf0d1ec41bd6412925746b90a1160d4e2f0
Rodrigodelacrz <rodrigo.delacruz.piconn@gmail.com>
2024-09-24 18:17:29 -0500

    Carga inicial del proyecto

---
+++ src/main/java/pe/edu/cibertec/examen_frontend/ExamenFrontendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.examen_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/examen_frontend/ExamenFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.examen_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
